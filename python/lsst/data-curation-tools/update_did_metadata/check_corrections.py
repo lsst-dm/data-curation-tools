a0e0bd36113282ca5ca69a939c84ec97fad7529c
Dennis Lee <dylee@fnal.gov>
2025-12-03 12:23:46 -0800

    Update scripts to accept arguments. Added README.md

--- python/lsst/data-curation-tools/update_did_metadata/check_corrections.py
+++ python/lsst/data-curation-tools/update_did_metadata/check_corrections.py
@@ -1,7 +1,7 @@
 import json
 
 
-with open('20251001_corrections.json', 'r', encoding='utf-8') as f:
+with open('20251101_corrections.json', 'r', encoding='utf-8') as f:
     data = json.load(f)
 
 
@@ -12,7 +12,7 @@ for did in data:
 
     different = False
     diff_keys = []
-    for k,v in old.items():
+    for k, v in old.items():
         if did[k] != v:
             diff_keys.append(k)
             different = True
--- python/lsst/data-curation-tools/update_did_metadata/gather_metadata.py
+++ python/lsst/data-curation-tools/update_did_metadata/gather_metadata.py
@@ -1,37 +1,50 @@
 #!/usr/local/env python3
+'''
+gather_metadata.py
 
+Script that gathers DID metadata, provided a file with a list of DIDs.
+The script gathers metadata and PFNs from Rucio,
+then uses the gfal client to get metadata from the PFNs.
+
+Copyright 2025 Fermi National Accelerator Laboratory (FNAL)
+'''
+import argparse
 import json
 import pprint
 
 import gfal2
 from rucio.client import Client
 
-c = Client()
+client = Client()
 ctx = gfal2.creat_context()
 
 
-def read_did_file():
+def read_did_file(file: str):
+    """
+    Reads a list of dids from a file that need updating.
+    """
     dids = []
-    with open('data/20251006_missing.txt', 'r', encoding='utf-8') as f:
+    with open(file, 'r', encoding='utf-8') as f:
         lines = f.readlines()
         for line in lines:
             if 'raw' in line:
-                did = line.split(' ')[0]
+                did = line.split(' ')[0].strip()
+                print(did)
                 dids.append(did)
     return dids
 
 
 def gather_metadata(dids):
     """
-    Gather metadata from Rucio and from using gfal2
+    Gather metadata from Rucio and gfal2
     """
     metadata = []
     for did in dids:
         scope, name = did.split(':')
-        did_meta = c.get_metadata(scope=scope, name=name)
+        did_meta = client.get_metadata(scope=scope, name=name)
 
-        replicas = c.list_replicas(dids=[{'scope': scope, 'name': name}],
-                                   rse_expression='SLAC_RAW_DISK')
+        replicas = client.list_replicas(dids=[{'scope': scope, 'name': name}],
+                                        rse_expression='SLAC_RAW_DISK')
         replica = list(replicas)[0]
         pfns = replica['pfns']
         for pfn, info in pfns.items():
@@ -55,11 +68,34 @@ def gather_metadata(dids):
 
 
 def main():
-    dids = read_did_file()
-    metadata = gather_metadata(dids)
+    parser = argparse.ArgumentParser(
+        description=("A utility that fetches DID metadata from a list of DIDs"
+                     "and saves updated metadata corrections."))
 
-    with open('20251006_corrections.json', 'w', encoding='utf-8') as f:
-        json.dump(metadata, f, indent=4)
+    parser.add_argument(
+        'dids_file',
+        type=str,
+        help='File containing the list of DIDs on each line'
+    )
+
+    parser.add_argument(
+        'corrections_file',
+        type=str,
+        help='File path to save the generated corrections'
+    )
+
+    args = parser.parse_args()
+
+    try:
+        dids = read_did_file(args.dids_file)
+        metadata = gather_metadata(dids)
+
+        with open(args.corrections_file, 'w', encoding='utf-8') as f:
+            json.dump(metadata, f, indent=4)
+    except FileNotFoundError as e:
+        print(f"Error: File not found: {e}")
+    except Exception as e:
+        print(f"An unexpected error occurred: {e}")
 
 
 if __name__ == '__main__':
